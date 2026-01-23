a9c2f14d039cc01a0f1df3ebeb44dc9f83e38d3d
陈健聪cjc <chenjiancong@sutpc.com>
2026-01-23 18:10:06 +0800

    图片改成icon 渲染

--- src/views/NotFound/index.vue
+++ src/views/NotFound/index.vue
@@ -1,15 +1,20 @@
 <template>
  <div class="not-found">
-  <img src="./svgs/404.svg" alt="404" />
+  <!-- <img src="./svgs/404.svg" alt="404" /> -->
+   <Icon icon="svg-icon:404" size="780" />
  </div>
 </template>
 <script setup lang="ts">
+  import Icon from "@cjc/vue3-svg-icon";
 
 </script>
 <style lang="less" scoped>
 .not-found {
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   img {
     width: 100%;
     height: 100%;
